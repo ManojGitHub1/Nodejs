a86a73c81dd7e87b1f378576de8cc9119175957a
ManojGitHub1 <manojjivanagi@gmail.com>
2024-08-18 01:31:26 +0530

    learning

--- index.js
+++ index.js
@@ -48,6 +48,8 @@
 // Node-mon
 // console.log("This Nodemon is Wonderfull!")
 
+
+
 // const http = require('http');
 // const data = require('./data')
 // http.createServer((req, res)=>{
@@ -59,28 +61,67 @@
 
 // Getting input from command line
 // console.log(process)
+// node ./index.js hello , so hello is argv[2] now
 // console.log(process.argv[2])
 
 
 
-
-// const fs = require('fs')
-
-// const input = process.argv;
+const fs = require('fs')
+const input = process.argv;
 // 1-argument file name, 2-argument content in file as input is array
 // fs.writeFileSync(input[2], input[3])
-// if(input[2]=='add'){
-//     fs.writeFileSync(input[3], input[4])
-// }else if(input[2]=='remove'){
-//     fs.unlinkSync(input[3]);
-// }else{
-//     console.log("Invalid input");
-// }
+if(input[2]=='add'){
+    fs.writeFileSync(input[3], input[4])
+}else if(input[2]=='remove'){
+    fs.unlinkSync(input[3]);
+    console.log("hi")
+}else{
+    console.log("Invalid input");
+}
 // node ./index.js apple.txt "This is a file created by fs,\n apple is fruit"
 // node ./index.js add orange.txt "This is color & fruit"                         
 // node ./index.js remove add   
 
 
 
+// const { error } = require('console');
+// const fs = require('fs')
+// const path = require('path')
+// const dirPath = path.join(__dirname, 'files');
+// console.warn(dirPath);
+// for(i=0; i<5; i++){
+//     fs.writeFileSync(dirPath+`/hello${i}.txt`, `This is ${i}th file.`)
+// }
 
+// fs.readdir(dirPath, (error, files)=>{
+//     // console.warn(files); // gives array
+//     files.forEach((items)=>{
+//         console.log("File name is",items);
+//     })
+// })
 
+// const dirPathold = path.join(__dirname, 'apple.txt');
+// console.log(dirPathold)
+// console.log(dirPath)
+
+// var oldPath = dirPathold
+// var newPath = dirPath
+
+// fs.rename(oldPath, newPath, function (err) {
+//     if (err) return console.log(err); 
+//     console.log('Successfully renamed - AKA moved!')
+// })
+
+// using fs-extra package
+// const fse = require('fs-extra')
+
+// const src = dirPathold
+// const dest = dirPath
+// const src = 'orange.txt'
+// const dest = 'files/orange.txt'
+
+// // using fse not fs
+// fse.move(src, dest, (err) => { 
+//     if (err) return console.log(err); 
+//     console.log(`File successfully moved!!`); 
+// });
